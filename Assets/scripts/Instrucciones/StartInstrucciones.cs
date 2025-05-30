0dfa7707f52166a317a1434155cf287b8d2b052d
JuanMantilla <est.juand.mantilla@unimilitar.edu.co>
2025-05-29 23:28:56 -0500

    Conecta todo
    Conecta todo

---
+++ Assets/scripts/Instrucciones/StartInstrucciones.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class StartInstrucciones: MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
